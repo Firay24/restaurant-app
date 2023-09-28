eb021699b536f12925b0d177d2b79ec80ce43a76
Hatakee Fira <fisan.onodaconan@gmail.com>
2023-09-28 22:06:27 +0700

    config tailwind, eslint, folder structure

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -9,4 +11,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
